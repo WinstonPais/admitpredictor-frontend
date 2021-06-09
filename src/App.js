8d5b69da69a7f129cd3ea87a0b32c6af3d779887
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2021-06-09 23:09:45 +0530

    Updated to include router and history
    1) installed react-router
    2) installed history

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+// import NavBar from './NavBar/NavBar';
+// import UserProvider from './contexts/UserProvider';
+import Home from './Pages/Home/Home';
+// import MakeRecipesPage from './MainPages/MakeRecipesPage/MakeRecipesPage';
+// import AllRecipesPage from './MainPages/AllRecipes/AllRecipes';
+// import PageNotFound from './MainPages/PageNotFound/PageNotFound';
+// import ContactPage from './MainPages/ContactPage/ContactPage';
+// import RecipeDetailsPage from './MainPages/RecipeDetailsPage/RecipeDetailsPage';
+import { Router, Route, Switch } from 'react-router';
+import { createBrowserHistory } from 'history';
+// import ScrollToTop from './ScrollToTop/ScrollToTop';
+
+const history = createBrowserHistory();
+
+class App extends Component {
+  render() {
+    return (
+    //   <UserProvider>
+        // <>
+        // <Router history={history}>
+        //     <Switch >
+        //       <Route path="*" component={NavBar}/>
+        //     </Switch>
+        // </Router>
+        // </>
+        <>
+        <Router history={history}>
+            {/* <ScrollToTop /> */}
+            <Switch >
+              <Route path="/admitpredictor-frontend" component={Home} exact />
+              {/* <Route path="/itadakimasu/makerecipepage" component={MakeRecipesPage} exact />
+              <Route path="/recipe/:id" component={RecipeDetailsPage} />
+              <Route path="/allrecipes" component={AllRecipesPage} />
+              <Route path="/contact" component={ContactPage} />
+              <Route component={PageNotFound} /> */}
+            </Switch>
+        </Router>
+        </>
+       
+    // </UserProvider>
+    )
+  }
+}
+
+export default App;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import Footer from './../../footer/footer';
+// import classes from './Home.module.css';
+// import HomeCarousel from './../../homecarousel/homecarousel';
+// import Grid from './../../Grid/Grid';
+// import { Container } from 'reactstrap';
+// import CreateRecipes from './../../createRecipes/createRecipes';
+
+const Home = () => {
+    return (
+        <div>Hello World MF</div>
+        // <Fragment>
+        //     <HomeCarousel/>
+        //     <div className={classes.makePositionRelative}>
+        //     <Container className={classes.gridTopMargin}>
+        //         <h4 className={[classes.whiteText,classes.featured].join(' ')}>&nbsp;&nbsp;Featured &nbsp;&nbsp;</h4>
+        //         <Grid/>
+        //     </Container>
+        //     <CreateRecipes />
+        //     <Footer />
+        //     </div>
+        // </Fragment>
+    )
+}
+
+export default Home
