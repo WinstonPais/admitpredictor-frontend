364dbce265c2b509e92a7cb43b4eb5d6c7ee67ac
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2021-06-17 22:06:10 +0530

    Moved FormPage file to its own Folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Home from './Pages/Home/Home';
 import GradHatLoader from './components/Loader/GradHatLoader';
-import FormPage from './Pages/Home/Formpage';
+import FormPage from './Pages/FormPage/Formpage';
 import { Router, Route, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
 
